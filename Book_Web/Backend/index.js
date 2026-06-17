a63f407a4ec040c08e9f44f33d459a8c8d71c8d9
Neera <neerakhatri003@gmail.com>
2026-06-17 22:37:31 +0545

    Complete restructure: Book_Web frontend + backend setup

---
+++ Book_Web/Backend/index.js
@@ -0,0 +1,26 @@
+const express =require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+//test route
+app.get("/", (req,res)=>{
+  res.send("Backend is running");
+})
+
+//connect db
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> console.log("MongoDB connected"))
+.catch((err)=> console.log(err));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{ 
+  console.log(`Server is running on port ${PORT}`);
+});
