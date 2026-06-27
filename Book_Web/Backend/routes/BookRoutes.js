b87fa6e79bd8cbbf301bcb03e7b07cda5371991e
Neera <neerakhatri003@gmail.com>
2026-06-27 22:30:47 +0545

    added post book routes

--- Book_Web/Backend/routes/BookRoutes.js
+++ Book_Web/Backend/routes/BookRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Book = require('../modles/Book'); 
+const Book = require('../models/Book'); 
 
 
 router.get("/",async (req,res)=>{
@@ -11,6 +11,19 @@ router.get("/",async (req,res)=>{
   catch(err){
     res.status(500).json({message:err.message})
   }
+})
+
+router.post("/",async (req,res)=>{
+  try{
+    const book= req.body;
+    const newBook = new Book(book);
+    await newBook.save();
+    res.status(201).json(newBook);
+  }
+  catch(err){
+    res.status(400).json({message:err.message})
+  }
+
 })
 
 module.exports = router;
