708e1fdd27f6584011940f56252e4e6a4cd58eab
Neera <neerakhatri003@gmail.com>
2026-06-29 23:21:17 +0545

    added update and delete routes

--- Book_Web/Backend/routes/BookRoutes.js
+++ Book_Web/Backend/routes/BookRoutes.js
@@ -29,12 +29,45 @@ router.post("/",async (req,res)=>{
     try{
       const book = await Book.findById(req.params.id);
       if(book==null){
-        return res.status(404).json(message="Book not found")
+        return res.status(404).json({message:"Book not found"})
       }
       res.json(book)
     }
     catch(err){
       res.status(404).json({message:err.message})
+    }
+  })
+
+  router.put("/:id", async(req,res)=>{
+    try{
+      const book = await Book.findById(req.params.id);
+      if(book==null){
+        return res.status(404).json({message:"Book not found"})
+      }
+      Object.assign(book, req.body);
+      await book.save();
+      res.json(book);
+    }
+    catch(err){
+      res.status(400).json({message:err.message})
+    }
+    
+
+  })
+
+
+  router.delete("/:id", async(req,res)=>{
+    try{
+      const book = await Book.findById(req.params.id);
+      if(book==null){
+        return res.status(400).json({message:"Book not found"})
+      }
+      await book.remove();
+      res.json({message:"Book deleted successfully"})
+      
+    }
+    catch(err){
+      res.status(500).json({message:err.message})
     }
   })
 
